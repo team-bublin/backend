e9339a0d4e1825fdf1d990b76451c402cb34e5d1
신수웅 <sinsuung@wemakeprice.com>
2023-03-26 14:57:59 +0900

    chore : JWT 의존성 추가

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -6,4 +6,7 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
